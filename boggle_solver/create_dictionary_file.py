deaada327719d7dbe430679b7ba5f43f3f8f9cb9
Danny Altkorn <daltkorn5@gmail.com>
2020-03-22 09:16:14 -0700

    updated dictionary file to use a trie

--- boggle_solver/create_dictionary_file.py
+++ boggle_solver/create_dictionary_file.py
@@ -1,43 +1,37 @@
-#!/usr/bin/python
-
+#!/Users/daltkorn5/compsci/anaconda3/bin/python
 import json
 
-def main():
-
-    words = get_words()
-    words_dict = arrange_words_into_dict(words)
-    write_file(words_dict)
 
 def get_words():
-
     with open('dictionary.txt') as f:
         words = f.read().split('\n')
 
     return words
 
-def arrange_words_into_dict(words):
 
-    words_dict = {}
-
+def arrange_words_into_trie(words):
+    root = {}
     for word in words:
-        if len(word) < 3: continue # don't care about words less than 3 letters long in boggle!
+        # we don't care about words with fewer than 3 letters in Boggle
+        if len(word) < 3:
+            continue
+        current_dict = root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict['end'] = True
+    return root
 
-        first_letter = word[:1]
-        if first_letter not in words_dict:
-            words_dict[first_letter] = {}
-
-        first_two_letters = word[:2]
-        if first_two_letters not in words_dict[first_letter]:
-            words_dict[first_letter][first_two_letters] = {}
-
-        words_dict[first_letter][first_two_letters][word.lower()] = 1
-
-    return words_dict
 
 def write_file(words_dict):
-
     with open('dictionary.json', 'w') as f:
         f.write(json.dumps(words_dict))
+
+
+def main():
+    words = get_words()
+    trie = arrange_words_into_trie(words)
+    write_file(trie)
+
 
 if __name__ == '__main__':
     main()
